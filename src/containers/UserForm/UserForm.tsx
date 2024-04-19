355893232d81f0ff1dcc9f866e89c632c8dbe67e
Renaud Dubuis <renaud.dubuis@decryptage.net>
2024-04-19 14:58:35 +0200

    Update

--- src/containers/UserForm/UserForm.tsx
+++ src/containers/UserForm/UserForm.tsx
@@ -1,12 +1,40 @@
 import React, { FC } from 'react';
+import { useForm } from 'react-hook-form';
 
 
-interface UserFormProps {}
+interface UserFormProps { }
 
-const UserForm: FC<UserFormProps> = () => (
-  <div data-testid="UserForm">
-    UserForm Component
-  </div>
-);
+const UserForm: FC<UserFormProps> = () => {
+
+  const { register, handleSubmit, watch, formState } = useForm();
+  const onSubmit = (data: object) => console.log(data);
+
+  console.log(watch("name"));
+
+  console.log(formState)
+
+  return (
+
+    <form onSubmit={handleSubmit(onSubmit)} data-testid="UserForm" style={{ display: 'flex', flexDirection: 'column' }}>
+
+      <input defaultValue="test" {...register("name")} />
+
+      <input {...register("email", {
+        required: true,
+        validate: (value, formValues) => {
+          console.log(formValues)
+          return value.length > 3
+        }
+      }
+      )} />
+
+      {formState.errors.email && <span>This field is required</span>}
+
+      <input type="submit" />
+
+    </form>
+  );
+
+};
 
 export default UserForm;
